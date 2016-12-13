b73cdfb9380fef49a606e0bb91f7b12886a4669d
gongyousan <gongyousan@jikexueyuan.com>
2016-12-13 22:09:23 +0800

    update to v1.5.0

---
+++ pull-docker-liberary.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+images=(kube-proxy-amd64:v1.5.0 kubedns-amd64:1.9 kube-scheduler-amd64:v1.5.0 kube-controller-manager-amd64:v1.5.0 kube-apiserver-amd64:v1.5.0 kube-dnsmasq-amd64:1.4 exechealthz-amd64:v1.2.0)
+for imageName in ${images[@]} ; do
+  docker pull jicki/$imageName
+  docker tag jicki/$imageName 123.206.77.131:5000/google_containers/$imageName
+  docker rmi jicki/$imageName
+done
