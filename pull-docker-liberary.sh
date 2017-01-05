fedb0c6a1f9ea2a3539867b6bbfbe87580e9605b
gongyousan <gongyousan@jikexueyuan.com>
2017-01-05 11:50:31 +0800

    Upgrade to 1.5.1

--- pull-docker-liberary.sh
+++ pull-docker-liberary.sh
@@ -2,6 +2,6 @@
 images=(kube-proxy-amd64:v1.5.1 kubedns-amd64:1.9 kube-scheduler-amd64:v1.5.1 kube-controller-manager-amd64:v1.5.1 kube-apiserver-amd64:v1.5.1 kube-dnsmasq-amd64:1.4 exechealthz-amd64:v1.2.0)
 for imageName in ${images[@]} ; do
   docker pull gysan/$imageName
-  docker tag gysan/$imageName gcr.io/google-containers/google_containers/$imageName
+  docker tag gysan/$imageName gcr.io/google-containers/$imageName
   docker rmi gysan/$imageName
 done
